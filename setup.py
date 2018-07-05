0c4ed9a04548ec8a8ef65a94dc11f5b3af482c9f
Gustavo Olegário <gustavo.olegario@dev.getmore.com.br>
2018-07-05 13:43:14 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyusuggest',
+      version='0.1',
+      description='API to integrate Ubersuggest with Python',
+      url='https://github.com/Olegario96/pyusuggest',
+      author='Gustavo Olegario',
+      author_email='gustavo-olegario@hotmail.com',
+      license='MIT',
+      packages=['pyusuggest'],
+      zip_safe=False)
