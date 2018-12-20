86d89b5ebd41234e809c5c6be85339e8eeae6f4b
Gustavo Olegário <gustavo-olegario@hotmail.com>
2018-12-19 22:05:31 -0200

    Using readme to create landing page on pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+from os import path
 from setuptools import setup
+
+pwd = path.abspath(path.dirname(__file__))
+with open(path.join(pwd, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='pyusuggest',
       version='1.0',
       description='API to integrate Ubersuggest with Python',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/Olegario96/pyusuggest',
       author='Gustavo Olegario',
       author_email='gustavo-olegario@hotmail.com',
